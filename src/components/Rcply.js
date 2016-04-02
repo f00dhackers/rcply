6f832c02ee3fe91be993347619108f9dc206802f
Gustavo Jimenez <gustavojimenez.folta@gmail.com>
2016-04-02 20:21:46 +0200

    refactor(navigation): move navigation markup to `Navigation` component

--- src/components/Rcply.js
+++ src/components/Rcply.js
@@ -2,39 +2,16 @@ import React, {
   Component,
   StyleSheet,
   View,
-  Text,
-  ScrollView
 } from 'react-native';
 
-import TabBar from './TabBar';
-import ScrollableTabView from 'react-native-scrollable-tab-view';
-import RecipesPage from './RecipesPage';
-import IngredientsPage from './IngredientsPage';
-import ShoppingListPage from './ShoppingListPage';
-import MorePage from './MorePage';
+import Navigation from './Navigation';
 
 class Rcply extends Component {
 
   render() {
     return (
       <View style={styles.container}>
-        <ScrollableTabView
-            initialPage={0}
-            renderTabBar={() => <TabBar />}
-        >
-          <ScrollView tabLabel="list-alt">
-            <RecipesPage />
-          </ScrollView>
-          <ScrollView tabLabel="home" >
-            <IngredientsPage />
-          </ScrollView>
-          <ScrollView tabLabel="shopping-basket">
-            <ShoppingListPage />
-          </ScrollView>
-          <ScrollView tabLabel="bars">
-            <MorePage />
-          </ScrollView>
-        </ScrollableTabView>
+        <Navigation />
       </View>
     );
   }
