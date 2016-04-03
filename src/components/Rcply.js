f021674ef10456403407bacd041db8fe3e49026d
Martin Breuer <martin@plantsandmachines.de>
2016-04-03 02:44:11 +0200

    sufficient camera access

--- src/components/Rcply.js
+++ src/components/Rcply.js
@@ -1,209 +1,68 @@
+// don't know if package.json will load everything you need
+// check out: https://github.com/lwansbrough/react-native-camera
+
+'use strict';
 import React, {
+  AppRegistry,
   Component,
+  Dimensions,
   StyleSheet,
-  View,
   Text,
-  DrawerLayoutAndroid
+  TouchableHighlight,
+  View
 } from 'react-native';
 
+import Camera from 'react-native-camera';
 
-// +++ FILTER TOGGLE BUTTON +++
 
-const { SwitchAndroid } = React;
-const Switch = React.createClass({
-  getInitialState() {
-    return {
-      value: false,
-    };
-  },
-
-  _onValueChange(value) {
-    this.setState({value: value});
-    if (this.props.onValueChange) {
-      this.props.onValueChange(value);
-    }
-  },
+class Cameraw extends Component {
 
   render() {
     return (
-      <SwitchAndroid onValueChange={this._onValueChange} value={this.state.value}/>
-    );
-  }
-});
-
-
-class Filter extends Component {
-  render() {
-
-    // +++ FILTER DRAWER +++
-
-    const filterDrawer = (
-      <View style={styles.filterDrawer}>
-
-        <Text style={styles.drawerHead}> Meal Type </Text>
-        <View style={styles.hr} />       
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> Appetizer </Text>
-          <Switch ref={'Appetizer'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }} value={true} />
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}  
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+        </Camera>
+        <View style={styles.capture}>
+          <Text style={styles.text} onPress={this.takePicture.bind(this)}>Take Picture</Text>
         </View>
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> Main Course </Text>
-          <Switch ref={'MainCourse'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }}/>
-        </View>
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> Dessert </Text>
-          <Switch ref={'Dessert'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }}/>
-        </View>
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> Snack </Text>
-          <Switch ref={'Snack'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }}/>
-        </View>
-
-        <Text style={styles.drawerHead}> Category </Text>
-        <View style={styles.hr} />       
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> Gluten Free </Text>
-          <Switch ref={'GlutenFree'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }}/>
-        </View>
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> Under 30 minutes </Text>
-          <Switch ref={'Under30min'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }}/>
-        </View>
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> Eat First </Text>
-           <Switch ref={'EatFirst'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }}/>
-        </View>
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> Spicy </Text>
-          <Switch ref={'Spicy'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }}/>
-        </View>
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> Extra Spicy </Text>
-          <Switch ref={'ExtraSpicy'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }}/>
-        </View>
-
-        <View style={styles.toggleBtn}> 
-          <Text style={styles.drawerSub}> One Pot Wonder </Text>
-          <Switch ref={'OnePotWonder'} style={styles.toggleBtn} onValueChange={(val) => {console.log(val); }}/>
-        </View>
-
-        <View style={styles.drawerBtnPanelWrapper}>
-        <View style={styles.drawerBtnPanel}>
-          <Text style={styles.textBtn} onPress={() => {
-            this.refs['Appetizer'].setState({value: false});
-            this.refs['MainCourse'].setState({value: false});
-            this.refs['Dessert'].setState({value: false});
-            this.refs['Snack'].setState({value: false});
-            this.refs['GlutenFree'].setState({value: false});
-            this.refs['Under30min'].setState({value: false});
-            this.refs['EatFirst'].setState({value: false});
-            this.refs['Spicy'].setState({value: false});  
-            this.refs['ExtraSpicy'].setState({value: false});
-            this.refs['OnePotWonder'].setState({value: false});
-
-          }}> Reset </Text>
-          <Text style={styles.textBtn} onPress={() => this.refs['Filter_Drawer'].closeDrawer()}> Accept </Text>
-        </View>
-        </View>
-
       </View>
-    );
+    );  
+  }
 
-    // +++ RECIPE SEARCH +++
-  
-    return (
-
-      <DrawerLayoutAndroid
-        drawerWidth={300}
-        ref={'Filter_Drawer'}
-        drawerPosition={DrawerLayoutAndroid.positions.Right}
-        renderNavigationView={() => filterDrawer}
-      >
-  
-        <View style={styles.recipeSearch}>
-          <Text style={styles.recipeSearchText}>Here goes the recipe search</Text>
-        </View>
-
-      </DrawerLayoutAndroid>
-    ); 
+  takePicture() {
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
   }
 }
 
-
-// +++ STYLES +++
-
 const styles = StyleSheet.create({
-
-  filterDrawer: {
-    flex: 1,
-    paddingLeft: 15,
-    paddingRight: 15,
-    backgroundColor: '#e6e6e6'
+  container: {
+    flex: 1
+  },  
+  preview: {
+    marginTop: 50,
+    alignItems: 'center',
+    height: Dimensions.get('window').height-150,
+    width: Dimensions.get('window').width
+  },  
+  capture: {
+    paddingTop: 25,
+    justifyContent: 'center', 
+    alignItems: 'center', 
   },
-
-  drawerHead: {
-    marginTop: 20,
-    marginBottom: 10,
-    fontSize: 16,
-    textAlign: 'left',
-    borderBottomWidth: 2
-  },
-
-  hr: {
-    height: 1,
-    backgroundColor: '#ddd',
-    marginBottom: 15
-  },
-
-  drawerSub: {
-    marginBottom: 15,
-    fontSize: 14,
-    textAlign: 'left'
-  },
-
-  toggleBtn: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-
-  drawerBtnPanelWrapper: {
-    //position: 'absolute',
-    bottom: -75,
-  },
-
-  drawerBtnPanel: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-
-  textBtn: {
-    fontSize: 16,
-    borderWidth: 2,
-    borderRadius: 10,
-    borderColor: '#fff'
-    //backgroundColor: '#ff0000'
-  },
-
-  recipeSearch: {
-    flex: 1,
-    backgroundColor: '#e6e6e6'
-  },
-
-  recipeSearchText: {
-    margin: 10,
+  text: {
     textAlign: 'center',
-    fontSize: 16
+    fontSize: 20,
+    color: '#000'
   }
-
 });
 
-export default Filter;
+AppRegistry.registerComponent('Cameraw', () => Cam);
+
+export default Cameraw;
